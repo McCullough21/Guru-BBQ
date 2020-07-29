41b0a010bff694f6bd9165bf99669da110680880
github username <github email address>
2020-07-29 09:18:01 -0400

    started to construct store

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+const store = createStore(*****Reducer, applyMiddleware(thunk));
+
 
 ReactDOM.render(
-  // <Provider store={store}>
+  <Provider store={store}>
   <App />,
-  // </Provider>,
+  </Provider>,
   document.getElementById("root")
 );
 
