b7a7b08c4fac21feb491e5df8cf67996a152deb0
github username <github email address>
2020-07-30 10:13:03 -0400

    css for app.js page buttons, link/route for rubs/sauces

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,28 +2,38 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import Smokers from "./components/smokers";
+import Recipes from "./components/rubs_sauces";
 
 class App extends Component {
   render() {
     return (
-      <div className="App-header">
-        <h1>Guru BBQ</h1>
-        <Router>
-          <nav>
-            <h3>
-              <Link to="/Smokers">
-                <button className="buttons">Smokers</button>
-              </Link>
-              <button className="buttons">Meats</button>{" "}
-              <button className="buttons">Rubs / Sauces</button>{" "}
-            </h3>
-          </nav>
-          <Switch>
-            <Route path="/Smokers">
-              <Smokers />
-            </Route>
-          </Switch>
-        </Router>
+      <div>
+        <div className="App-header">
+          <h1>Guru BBQ</h1>
+        </div>
+        <div>
+          <Router>
+            <nav>
+              <h3 className="buttons">
+                <Link to="/Smokers">
+                  <button className="button">Smokers</button>
+                </Link>{" "}
+                <button className="button">Meats</button>{" "}
+                <Link to="/Rubs&Sauces">
+                  <button className="button">Rubs / Sauces</button>{" "}
+                </Link>
+              </h3>
+            </nav>
+            <Switch>
+              <Route path="/Smokers">
+                <Smokers />
+              </Route>
+              <Route path="/Rubs&Sauces">
+                <Recipes />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
       </div>
     );
   }
---
+++ react-frontend/src/components/rubs_sauces.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Recipes() {
+  return <h3>Recipes</h3>;
+}
+
+export default Recipes;
