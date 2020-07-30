3547386f610c8c093e5a074e9df4cdda69e63069
github username <github email address>
2020-07-30 08:58:57 -0400

    started stubbing out app.js serving as home page

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -4,8 +4,13 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App-header">
         <h1>Guru BBQ</h1>
+        <h3>
+          <button className="buttons">Smokers</button>{" "}
+          <button className="buttons">Meats</button>{" "}
+          <button className="buttons">Rubs / Sauces</button>{" "}
+        </h3>
       </div>
     );
   }
--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -2,17 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+// import { Provider } from "react-redux";
+// import { createStore, applyMiddleware } from "redux";
+// import thunk from "redux-thunk";
 
-const store = createStore(*****Reducer, applyMiddleware(thunk));
-
+// const store = createStore(Reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <Provider store={store}>
+  // <Provider>
   <App />,
-  </Provider>,
+  /* </Provider>, */
   document.getElementById("root")
 );
 
