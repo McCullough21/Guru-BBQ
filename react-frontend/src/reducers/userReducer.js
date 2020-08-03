daaf8196874ecab71fd2580e97cf22145d4c429b
github username <github email address>
2020-08-03 12:56:17 -0400

    started creating reducer

--- react-frontend/src/reducers/userReducer.js
+++ react-frontend/src/reducers/userReducer.js
@@ -2,3 +2,26 @@
 // will populate store with user info and comments
 // will post new comments made in MEATS Container
 // will post new user on SignUp
+const userReducer = (
+  state = {
+    user: {},
+    comments: []
+  },
+  action
+) => {
+  switch (action.type) {
+    case "POPULATE_USER":
+      return {
+        ...state,
+        user: action.info,
+        comments: [...state.comments]
+      };
+
+    case "POPULATE_COMMENTS":
+      return {
+        ...state,
+        user: { ...state.user },
+        comments: [action.comments]
+      };
+  }
+};
