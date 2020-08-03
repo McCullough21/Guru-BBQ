636484556a8b98f37330d520c056360588578b2c
github username <github email address>
2020-08-03 14:38:24 -0400

    started rails controllers

--- rails-backend/app/controllers/comments_controller.rb
+++ rails-backend/app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+
+    def index
+        @comments = Comment.all
+    end
+    
+    def create
+        @comment = Comment.create(content: params[:content], user_id: params[:id], user_username: params[:username])
+
+    end
 end
--- rails-backend/app/controllers/users_controller.rb
+++ rails-backend/app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+   def create
+        @user = User.create(username: params[:username], password: params[:password])
+    end
+
+    def show
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            render json: UserSerializer.new(@user).serialized_user
+        end
+    end
 end
