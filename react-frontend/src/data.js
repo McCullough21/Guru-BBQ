68785b51c69a46a8c18d70b321e9bef3b15276c6
github username <github email address>
2020-08-10 12:05:23 -0400

    data.js file created with two meats with data

---
+++ react-frontend/src/data.js
@@ -0,0 +1,14 @@
+let meats = [
+  {
+    type: "Pork Butt",
+    cooking:
+      "Apply generous amount of dry rub to pork butt and let sit for 15 minutes.  Set smoker to 260°-275°.  Smoke meat until internal temp reaches 160°-170°.  Wrap with two layers of aluminum foil, place back on smoker.  Maintain smoker temperature until meat reaches internal temp of 197°.  Remove from smoker and let sit for at least one hour before pulling apart.",
+    img: ""
+  },
+  {
+    type: "St. dLouis-Style Ribs",
+    cooking:
+      "Cover outside of rack with dry rub and let sit until the meat starts sweating.  Set smoker to 250°-275°.  Smoke meat for 3 hours or until meat reaches an internal temp of 160°-170°.  Wrap each rack in aluminum foil, place back on smoker and maintain temperature.  When ribs reach an internal temp of 205° remove and let rest for 30 minutes.",
+    img: ""
+  }
+];
