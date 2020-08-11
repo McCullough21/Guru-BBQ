d505b6e8e045357e39e34beee91d57b06181efa7
github username <github email address>
2020-08-11 11:16:06 -0400

    img src urls for meats images

--- react-frontend/src/data.js
+++ react-frontend/src/data.js
@@ -2,11 +2,13 @@ let porkButt = {
   type: "Pork Butt",
   cooking:
     "Apply generous amount of dry rub to pork butt and let sit for 15 minutes.  Set smoker to 260°-275°.  Smoke meat until internal temp reaches 160°-170°.  Wrap with two layers of aluminum foil, place back on smoker.  Maintain smoker temperature until meat reaches internal temp of 197°.  Remove from smoker and let sit for at least one hour before pulling apart.",
-  img: ""
+  img:
+    "https://images.squarespace-cdn.com/content/v1/54b2faf2e4b0c67d15add912/1449778932129-V4E90PBNTQSCVRVG92DP/ke17ZwdGBToddI8pDm48kHJjM-Evnp5g-1kf5Yv15cUUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKcpWKe3KzaCrFDKPR1a1Ob8xobjReaxMuaKtrvUDoDmPO9EsdBHei1w8jR6w0UZiby/Grill+Dome+Smoked+Pork+Butt?format=1500w"
 };
 let ribs = {
   type: "St. dLouis-Style Ribs",
   cooking:
     "Cover outside of rack with dry rub and let sit until the meat starts sweating.  Set smoker to 250°-275°.  Smoke meat for 3 hours or until meat reaches an internal temp of 160°-170°.  Wrap each rack in aluminum foil, place back on smoker and maintain temperature.  When ribs reach an internal temp of 205° remove and let rest for 30 minutes.",
-  img: ""
+  img:
+    "https://i8.amplience.net/i/traeger/20190628-St-Louis-BBQ-Ribs_RE_HE_M?w=620&sm=aspect&aspect=5:3&scaleFit=poi&$poi2$&fmt=webp"
 };
