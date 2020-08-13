19a4beef0cce52a0da746956cc4b83f2ca8c8fe2
github username <github email address>
2020-08-13 08:57:32 -0400

    date created added to comments

--- react-frontend/src/components/comments.js
+++ react-frontend/src/components/comments.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 function Comments(props) {
+  let date = new Date(props.comment.created_at);
+  let dateStamp = date.toDateString();
   return (
     <ul>
       <li>
-        <h3>{props.comment.user_username}</h3>
+        <h3>{props.comment.user_username}</h3> <p>{dateStamp}</p>
         <p>{props.comment.content}</p>
       </li>
     </ul>
