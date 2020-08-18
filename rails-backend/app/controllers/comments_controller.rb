5114859d2b4db228c427783f0948656fd62d9510
github username <github email address>
2020-08-18 10:27:40 -0400

    loging and signup form dynamic. need to create fetches and reducers

--- rails-backend/app/controllers/comments_controller.rb
+++ rails-backend/app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
         @comments = Comment.all
         render json: @comments.to_json
     end
-
+# is username needed, maybe for fetching comments to display in meats with user's name
     def create
         
         @comment = Comment.create(content: params[:content], user_id: params[:id], user_username: params[:username])
--- react-frontend/src/containers/User.js
+++ react-frontend/src/containers/User.js
@@ -2,7 +2,23 @@ import React from "react";
 
 class User extends React.Component {
   render() {
-    return <div>User</div>;
+    return (
+      <div>
+        <form>
+          <label for="name">Username:</label>
+          <input type="text" name="name" />
+          <br></br>
+          <label for="password">Password:</label>
+          <input type="text" name="password" />
+          <br></br>
+          <input
+            type="sumbit"
+            value={this.props.type}
+            style={{ width: "5%" }}
+          />
+        </form>
+      </div>
+    );
   }
 }
 
