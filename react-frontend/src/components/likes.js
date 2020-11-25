afa909c6195ebe89520c26fcd8bf1de7434af686
McCullough21 <michael.mccullough21@gmail.com>
2020-11-25 13:16:35 -0500

    changed around Likes component with style of rendering and state

--- react-frontend/src/components/likes.js
+++ react-frontend/src/components/likes.js
@@ -6,7 +6,9 @@ class Likes extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      likes: props.likes
+      likes: props.likes,
+      notLoggedIn: !props.user.username
+      // userHasLiked:
     };
   }
   // must be logged in to Like comment
@@ -14,15 +16,15 @@ class Likes extends React.Component {
   //      With DELETE fetch to API.
   // dispatch for new like and create to api
 
-  notLoggedIn = () => {
-    if (!this.props.user.username) {
-      return true;
-    }
-  };
-
   handleClick = event => {
-    console.log(event.target);
-    event.target.style.color = "blue";
+    // set state to liked
+    // trigger action that contains api calls
+    // if (this.notLoggedIn()) {
+    //   let notAbleToLike = this.abilityToLike();
+    //   return (notAbleToLike.style.color = "red");
+    // }
+    // console.log(event.target);
+    // event.target.style.color = "blue";
     //   if user hasliked? unlike / change button color back
     // if hasn't Like/change button color to blue
     //   ???Event.taget style ????
@@ -39,35 +41,21 @@ class Likes extends React.Component {
     }
   };
 
-  abilityToLike = () => {
-    if (this.notLoggedIn()) {
-      return <p>"*Login required*"</p>;
-    } else {
-      return null;
-    }
-  };
-
-  renderLikeButton = () => {
-    if (this.userHasLiked()) {
-      return (
-        <button onClick={this.handleClick} style={{ color: "blue" }}>
-          Likes: {this.state.likes.length}
-        </button>
-      );
-    } else {
-      return (
-        <button onClick={this.handleClick}>
-          Likes: {this.state.likes.length}
-        </button>
-      );
-    }
-  };
-
   render() {
     return (
       <>
-        {this.renderLikeButton()}
-        {this.abilityToLike()}
+        {this.userHasLiked() ? (
+          <button onClick={this.handleClick} style={{ color: "blue" }}>
+            Likes: {this.state.likes.length}
+          </button>
+        ) : (
+          <button onClick={this.handleClick}>
+            Likes: {this.state.likes.length}
+          </button>
+        )}
+        {this.state.notLoggedIn ? (
+          <p>*Login required to like a comment*</p>
+        ) : null}
       </>
     );
   }
