757c126528d366aa42867be1d6b61910cd710eeb
McCullough21 <michael.mccullough21@gmail.com>
2020-12-14 14:40:08 -0500

    styling of login/signup form... styling of smokers comp

--- react-frontend/src/components/smokers.js
+++ react-frontend/src/components/smokers.js
@@ -3,31 +3,44 @@ import { offSet, drumSmoker } from "../data";
 
 function Smokers() {
   return (
-    <div style={{ display: "table", height: "300px", width: "100%" }}>
-      <div
-        style={{
-          display: "column",
-          width: "45%",
-          float: "left",
-          padding: "15px"
-        }}
-      >
-        <h2>{offSet.type}</h2>
-        <img src={offSet.img} alt="offSet" style={{ width: "300px" }} />
-        <h4>{offSet.description}</h4>
+    // <div class="max-w-sm bg-white rounded-lg overflow-hidden shadow-lg">
+    //   <img class="w-full" src="/assets/docs/master/image-01.jpg" alt="Sunset in the mountains"/>
+    //     <div class="px-6 py-4">
+    //       <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+    //       <p class="text-gray-700 text-base">
+    //         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et
+    //         perferendis eaque, exercitationem praesentium nihil.
+    //             </p>
+    //     </div>
+    //     <div class="px-6 py-4">
+    //       <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+    //       <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+    //       <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+    //     </div>
+
+    //     </div>
+
+    <div className="flex mt-6">
+      <div className="flex-col">
+        <h2 className="text-4xl italic font-bold py-6 text-center">
+          {offSet.type}
+        </h2>
+        <div className="flex justify-center">
+          <img src={offSet.img} alt="offSet" className="max-w-lg" />
+        </div>
+        <h4 className="m-4 py-4 text-xl font-light">{offSet.description}</h4>
       </div>
 
-      <div
-        style={{
-          display: "column",
-          width: "45%",
-          float: "left",
-          padding: "15px"
-        }}
-      >
-        <h2>{drumSmoker.type}</h2>
-        <img src={drumSmoker.img} alt="offSet" style={{ width: "300px" }} />
-        <h4>{drumSmoker.description}</h4>
+      <div className="flex-col">
+        <h2 className="text-4xl italic font-bold py-6 text-center">
+          {drumSmoker.type}
+        </h2>
+        <div className="flex justify-center">
+          <img src={drumSmoker.img} alt="offSet" className="max-w-lg" />
+        </div>
+        <h4 className="m-4 py-4 text-xl font-light">
+          {drumSmoker.description}
+        </h4>
       </div>
     </div>
   );
--- react-frontend/src/containers/User.js
+++ react-frontend/src/containers/User.js
@@ -46,6 +46,7 @@ class User extends React.Component {
           onSubmit={event => {
             this.handleSubmit(event);
           }}
+          className="m-5"
         >
           <label>
             Username:
@@ -54,6 +55,8 @@ class User extends React.Component {
               name="username"
               value={this.state.username}
               onChange={event => this.handleChange(event)}
+              className="w-50 px-3 py-2 border m-2 border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="Username"
             />
           </label>
           <br></br>
@@ -64,10 +67,16 @@ class User extends React.Component {
               name="password"
               value={this.state.password}
               onChange={event => this.handleChange(event)}
+              className="w-50 px-3 py-2 border m-2 border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="Password"
             />
           </label>
           <br></br>
-          <input type="submit" value={this.props.actionType} />
+          <input
+            type="submit"
+            value={this.props.actionType}
+            className="ml-1 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          />
         </form>
       </div>
     );
