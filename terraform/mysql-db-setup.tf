05c4cf3f4da4a85fb03bfeb03290b44e45167f4a
McCullough21 <michael.mccullough21@gmail.com>
2021-04-07 10:58:58 -0400

    db instance creation works, network pretty much done

--- terraform/mysql-db-setup.tf
+++ terraform/mysql-db-setup.tf
@@ -3,18 +3,20 @@ resource "aws_db_subnet_group" "private" {
     subnet_ids = [aws_subnet.guru-subnet-private-1.id, aws_subnet.guru-subnet-private-2.id]
 }
 
-resource "aws_db_instance" "default" {
+
+resource "aws_db_instance" "guru-db-instance" {
   allocated_storage    = 20
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
-  name                 = "guru-bbq-db"
+  name                 = "gurubbqdb"
+  identifier = "gurubbqdb"
   username             = var.username
   password             = var.password
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
-  apply_immediately = true
-  db_subnet_group_name = aws_db_subnet_group.private.id 
-  publicaly_accessible = false
+  apply_immediately = false
+  db_subnet_group_name = aws_db_subnet_group.private.name
+  publicly_accessible = false
   vpc_security_group_ids = [aws_security_group.allow-internal.id]
 }
